3a33a0d006b724c2e6d86ea6ff20debd1335b00b
Nil Xandrea Montejo <montejo.nilxandrea@gmail.com>
2025-03-26 23:53:00 +0800

    First Push to Repository

---
+++ app/Http/Controllers/User1Controller.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Traits\ApiResponser;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use DB;
+
+class User1Controller extends Controller
+{
+    use ApiResponser;
+
+    private $baseUri;
+
+    public function __construct()
+    {
+        $this->baseUri = config('services.site1.base_url');
+    }
+
+    public function index()
+    {
+        // Clear all the codes inside the Controller Methods/Functions
+    }
+
+    public function add(Request $request)
+    {
+        // Clear all the codes inside the Controller Methods/Functions
+    }
+
+    public function show($id)
+    {
+        // Clear all the codes inside the Controller Methods/Functions
+    }
+
+    public function update(Request $request, $id)
+    {
+        // Clear all the codes inside the Controller Methods/Functions
+    }
+
+    public function delete($id)
+    {
+        // Clear all the codes inside the Controller Methods/Functions
+    }
+}
